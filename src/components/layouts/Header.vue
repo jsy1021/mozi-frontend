5c65a2dd2e513073661db7eb138501479b33267f
sunbread8355 <sunbread8355@gmail.com>
2025-07-21 12:45:19 +0900

    feat: 상단바, 하단바 추가

---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,33 @@
+<script setup></script>
+<template>
+  <div class="header">
+    <div class="logo">MoZi</div>
+
+    <div class="buttons">
+      <router-link :to="{ name: 'homePage' }"
+        ><i class="fa-solid fa-house"></i
+      ></router-link>
+      <router-link :to="{ name: 'hamburgerMenu' }"
+        ><i class="fa-solid fa-bars"></i
+      ></router-link>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.logo {
+  margin-left: 12px;
+}
+.buttons {
+  margin-right: 10px;
+}
+.buttons > * {
+  margin-right: 12px;
+  color: inherit;
+}
+</style>
