5b2f1dff3a53c159f20302b09102c5e8ede43d14
kyounmmi <mfpa02@naver.com>
2025-08-02 16:49:57 +0900

    feat : 라우터 기능 추가

--- src/router/user.js
+++ src/router/user.js
@@ -5,4 +5,45 @@ export default [
     component: () => import('../pages/user/MyPage.vue'),
     meta: { isHeader: true, isFooter: true },
   },
+  {
+    path: '/user/personal',
+    name: 'personal',
+    component: () => import('../pages/user/PersonalInfo.vue'),
+    meta: { isHeader: true, isFooter: true },
+  },
+  {
+    path: '/multi-step',
+    name: 'MultiStep',
+    component: MultiStep,
+    meta: { isHeader: true, isFooter: true },
+  },
+  {
+    path: '/user/edit-info',
+    name: 'EditInfo',
+    component: () => import('../pages/user/EditInfo.vue'),
+    meta: { isHeader: true, isFooter: true },
+  },
+  {
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://127.0.0.1:3000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  },
 ];
+
+import { createRouter, createWebHistory } from 'vue-router';
+import PersonalInfo from '../pages/user/PersonalInfo.vue';
+import MultiStep from '../pages/user/MultiStep.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/personal-info', name: 'PersonalInfo', component: PersonalInfo },
+    { path: '/multi-step', name: 'MultiStep', component: MultiStep },
+  ],
+});
