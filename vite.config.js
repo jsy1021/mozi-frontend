2d6f5eb5136bf4abb013b818950688941ca9ca06
jieunee32 <hsilver0302@gmail.com>
2025-08-02 20:34:42 +0900

    vite.config 삭제

--- vite.config.js
+++ vite.config.js
@@ -22,11 +22,10 @@ export default defineConfig({
     },
   },
   build: {
-    //'C:/dev/projects/mozi-backend/src/main/webapp/resources',
-    outDir:
-      //"D:/KB_6th/final_project/Mozi/mozi-backend/src/main/webapp/resources",
-      //'C:/KB-PJT/goal/mozi-backend/src/main/webapp/resources',
-      'C:/KB_fullstack/final_project/backend/0731/mozi-backend/src/main/webapp/resources',
+    outDir: //'C:/dev/projects/mozi-backend/src/main/webapp/resources',
+    //"D:/KB_6th/final_project/Mozi/mozi-backend/src/main/webapp/resources",
+    //'C:/KB-PJT/goal/mozi-backend/src/main/webapp/resources',
+    //'C:/KB_fullstack/final_project/backend/0728/mozi-backend/src/main/webapp/resources',
     //'C:/KB_Fullstack/accountConnect/mozi_4_backend/src/main/webapp/resources',
   },
 });
