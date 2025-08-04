663c7e2d5deb12f01a4c7ea4931107d8458daf66
ju1645 <74340864+ju1645@users.noreply.github.com>
2025-08-04 19:03:33 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -24,6 +24,6 @@ export default defineConfig({
   build: {
     //'C:/dev/projects/mozi-backend/src/main/webapp/resources',
     outDir:
-      'C:/KB_Fullstack/10_finalProject/mozi-backend/src/main/webapp/resources',
+      //'C:/KB_Fullstack/10_finalProject/mozi-backend/src/main/webapp/resources',
   },
 });
